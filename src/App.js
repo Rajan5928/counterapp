35e818f3d1c4e6a62fd4b9c4ecffc6624e3fae40
Rajan5928 <prasoonkumar887@gmail.com>
2020-03-04 19:30:00 +0530

    first commit for today

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state={
+    count:0,
+  }
+  incFun = () => {
+    const updatedState = this.state.count + 1;
+    this.setState({ count: updatedState })
+  }
+  decFun = () => {
+    const updatedState = this.state.count - 1;
+      this.setState({count: updatedState})
+  }
+  componentDidMount(){
+    Axios.get("https://5d76bf96515d1a0014085cf9.mockapi.io/playlist")
+    .then((response)=>{
+      console.log(response);
+    })
+    .catch((err)=>{
+      console.log("error");
+    })
+  }
+  render()   {
+    return (
+      <div className="wrapper" >
+        <div>
+          <h1 className="count">{this.state.count}</h1>
+        </div>
+        <div className="buttons">
+          <button className="inc" onClick={this.incFun}>Increment +1</button>
+          <button className="dec" onClick={this.decFun}>Decrement -1</button>
+        </div>
+      </div>
+    );
+  } 
+
 }
 
 export default App;
